4ee4baad4d150a22dbc9e635027be981abca4a72
Piotr Trocki <piotr@trocki.com>
2024-09-10 19:14:31 +0200

    Add metro config

--- metro.config.js
+++ metro.config.js
@@ -1,12 +1,35 @@
-const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 require('dotenv').config();
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('metro-config').MetroConfig}
- */
-const config = {};
+const path = require('path');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const MetroSymlinksResolver = require('@rnx-kit/metro-resolver-symlinks');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const defaultConfig = getDefaultConfig(__dirname);
+const {assetExts, blockList, sourceExts} = defaultConfig.resolver;
+
+const blockCustomExts = [];
+
+console.log(`🚀🚀🚀🚀🚀🚀🚀🚀 `, process.env.FEATURE_A);
+
+if (process.env.FEATURE_A === 'false') {
+  blockCustomExts.push(new RegExp(`.+.featurea`));
+}
+if (process.env.FEATURE_B === 'false') {
+  blockCustomExts.push(new RegExp(`.+.featureb`));
+}
+
+/**
+ *  type {import('metro-config').MetroConfig}
+ */
+const config = {
+  resolver: {
+    resolveRequest: MetroSymlinksResolver(),
+    assetExts: assetExts,
+    sourceExts: sourceExts,
+    blockList: [blockList, ...blockCustomExts],
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
